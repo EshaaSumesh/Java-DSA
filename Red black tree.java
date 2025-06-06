4f2ddbac463f977831aa89f021f9ee9fc7169751
Eshaa Sumesh <126137382+EshaaSumesh@users.noreply.github.com>
2025-06-06 14:31:30 +0530

    Create Red black tree.java

---
+++ Red black tree.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+class Main
+{
+    public static void main (String[]args)
+    
+    {
+        TreeSet<Integer> set=new TreeSet<>();
+        set.add(10);
+        set.add(5);
+        set.add(2);
+        System.out.print(set);
+    }
+}
