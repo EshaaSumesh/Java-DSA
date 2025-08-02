f9a83db33dd2046e2fbfe6d25331ebcd1d99b680
Eshaa Sumesh <126137382+EshaaSumesh@users.noreply.github.com>
2025-08-02 08:47:50 +0530

    Create I.java

---
+++ patterns/I.java
@@ -0,0 +1,18 @@
+class Main
+{
+    public static void main(String[] args) 
+    {
+        for(int i=0;i<5;i++)//row
+        {
+            for(int j=0;j<5;j++)//column
+            {
+                if(j==2 || i==4 || i==0)
+                System.out.print("* ");
+                else 
+                System.out.print("  ");
+            }
+             System.out.println();
+        }
+       
+    }
+}
